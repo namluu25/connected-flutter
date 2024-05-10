2b12931f761515eafaab65d805833a002bb39510
Nam Luu <namluu253@gmail.com>
2024-05-10 13:35:49 +0700

    refactor: update icon naming

--- assets/icons/ic_add.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-
-const String assetName = './add.svg';
-final Widget icAdd = SvgPicture.asset(
-    assetName,
-    semanticsLabel: 'Add icon'
-);
